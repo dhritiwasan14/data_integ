af131c85256dbf3c0489514290a1055698fa241d
arjun <dhriti_wasan@mymail.sutd.edu.sg>
2018-03-24 13:26:10 +0800

    new dir

---
+++ app/routes/routes.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+
+    return res.render('index');
+});
+
+
+router.post('/', function(req, res) {
+    console.log(req.username);
+});
+
+router.get('/signup', function(req, res) {
+    return res.render('register');
+});
+
+router.get('/login', function(req, res) {
+    return res.render('index');
+});
+
+router.get('/profile', function(req, res) {
+    return res.render('profile');
+});
+
+router.post('/signup', function(req, res) {
+    return res.render('index');
+});
+
+router.get('/logout', function(req, res) {
+    return res.render('index');
+});
+
+
+
+module.exports = router;
